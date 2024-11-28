009d898891767f22044266918b9a67de3b61a1ca
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-11-28 12:23:41 -0600

    implements the score and the correct number background color

--- script.js
+++ script.js
@@ -8,31 +8,47 @@
 // console.log(document.querySelector('.guess'));
 // document.querySelector('input .guess').textContent = 4;
 
-const hidden = Math.round(Math.random() * 20);
-console.log(hidden);
+const hiddenNumber = Math.round(Math.random() * 20);
+console.log(hiddenNumber);
 
-let score = Number(document.querySelector('.score').textContent);
-// let highscore = Number(document.querySelector('.highscore').textContent);
-let scoreArray = [];
+//Initialize the score
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //No Number guess
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
-  } else if (guess < hidden) {
-    document.querySelector('.message').textContent = 'Go up!';
-    score = score - 1;
-    // console.log(typeof score);
-  } else if (guess > hidden) {
-    document.querySelector('.message').textContent = 'Go down!';
-    score = score - 1;
-  } else {
-    document.querySelector('.message').textContent = 'Correct number!';
-    document.querySelector('.number').textContent = hidden;
-    scoreArray.push(score);
   }
-  document.querySelector('.score').textContent = score;
-  console.log(scoreArray);
+  //Correct guess
+  else if (guess === hiddenNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.number').textContent = hiddenNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+  }
+  //Wrong guess
+  else if (guess < hiddenNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess > hiddenNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  // document.querySelector('.score').textContent = score;
 });
